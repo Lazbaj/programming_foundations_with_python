a3f3d7c95c7e6e04deaa79aedff884218de83a1f
Lazbaj <lazbaj@gmail.com>
2016-05-30 19:43:02 +0200

    Write final version of rename_files.py

--- rename_files.py
+++ rename_files.py
@@ -1,5 +1,17 @@
 import os
 
-file_list = os.listdir('./prank')
-print file_list
+def rename_files(folder):
+    file_list = os.listdir(folder)
+    print file_list
+    saved_path = os.getcwd()
+    os.chdir(folder)
 
+    for e in file_list:
+        print 'Old file name: ' + e
+        os.rename(e,e.translate(None,'0123456789'))
+        print 'New file name ' + e.translate(None,'0123456789')
+        print
+
+    os.chdir(saved_path)
+
+rename_files('/Users/Lazbaj/Desktop/version-control/programming_foundations_with_python/prank')
